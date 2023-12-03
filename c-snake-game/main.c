cbcf8450ba8d1fa00891ee4246654a5d81a22441
Kenan <kenanbalija.dev@gmail.com>
2023-12-03 15:03:44 +0100

    Initial Commit

---
+++ c-snake-game/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-snake-game
+//
+//  Created by Kenan Balija on 3. 12. 2023..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
